5f2e36b55844f9223ea544ded954aaa73e17c2c7
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2025-05-28 10:49:39 -0700

    updates flower class for different flower designs
    adds flower growth function to "grow" flowers and their petals.

--- src/flower.js
+++ src/flower.js
@@ -4,40 +4,61 @@
 //This is then used to draw growing flowers onto a canvas in sketch.js
 
 class Flower {
-    constructor(x, y, size = 20, type = 'circle') {
-        this.x = x
-        this.y = y
-        this.size = size
-        this.type = type
-        this.health = 100
-        this.total_growth = 0
-        this.growth_rate = 0.3
+    constructor(x, y, size = 20, petals = 4, type = "circle") {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.type = type;
+        this.petals = petals;
+        this.health = 100;
+        this.total_growth = 0;
+        this.growth_rate = 1;
+        this.color = [0, 0, 0];
     }
 
     grow() {
-        if (this.size <= 200) {
-            this.size += this.growth_rate
+        if (this.total_growth <= 300) {
+            this.total_growth += this.growth_rate;
+            console.log(this.total_growth);
         }
     }
 
     isClicked(mouseX, mouseY) {
-        const D = dist(mouseX, mouseY, this.x, this.y)
-        return D < this.size / 2
+        const D = dist(mouseX, mouseY, this.x, this.y);
+        return D < this.size / 2;
     }
 
     draw() {
-        if (this.total_growth < 500) {
-            this.y -= 1
-            this.total_growth += 1
-            console.log(this.total_growth)
-        }
-        if (this.size > 0 && this.type === 'circle') {
-            fill(0)
-            circle(this.x, this.y, this.size)
-        }
+        if (this.total_growth < 300) {
+            this.y -= 1;
+            //console.log(this.total_growth);
+        } else return;
     }
 
     hide() {
-        this.size = 0
+        this.size = 0;
+    }
+
+    //logic from Kazuki Umeda: https://github.com/Creativeguru97/YouTube_tutorial/tree/master/Play_with_geometry/3DMathFlowers
+    draw_flower() {
+        if (this.total_growth < 250) {
+            return;
+        }
+        fill(0);
+        stroke(0);
+        strokeWeight(3);
+
+        beginShape(POINTS);
+        for (let phi = 0; phi < 360; phi += .5) {
+            let r = this.size * Math.sin(phi * this.petals);
+            let x = this.x + (r * Math.cos(phi));
+            let y = this.y + (r * Math.sin(phi));
+            vertex(x, y);
+        }
+        endShape();
+        if (this.size < 50) {
+            this.size += .1;
+        }
+        console.log(this.size);
     }
 }
